0eb176995fd6a5ddd964fdf86fe5e1e49a4d21aa
Sophia <66008240+sophiastasevic@users.noreply.github.com>
2021-11-29 14:11:34 +0000

    Update DerotFITSImage.py
    more descriptive comments + incorporated function for rotating single image (uncomment relevant lines to use this option)

--- DerotFITSImage.py
+++ DerotFITSImage.py
@@ -1,5 +1,5 @@
 #script to derotate data using angles from image header
-#create file called 'starnames.txt' with names of stars needing to be derotated with the name in the form 'HIP_#####' 
+#create test file called 'starnames.txt' with names of stars needing to be derotated with the name in the form 'HIP_#####' 
 
 import scipy 
 import numpy as np
@@ -8,15 +8,15 @@ from astropy.io import fits
 import warnings
 warnings.simplefilter("ignore")
 
-#reads in the names of the images
+#reads in list containing the file names of the individual science cube frames
 def ReadIn(name):
     
-    file_names=np.loadtxt('{name}_filenames.txt'.format(name=name),dtype=str) #names of individual frames
+    file_names=np.loadtxt('{name}_filenames.txt'.format(name=name),dtype=str)
     for i in range(0,file_names.size):
-        file_names[i] = file_names[i].rstrip()
+        file_names[i] = file_names[i].rstrip() #removes trailing characters
     return file_names
 
-#rotates each image in the centred cube by the angle taken from the header
+#reads in centered cube for the star and correct each frame using the angle calculated from the header
 def RotCube(name,angles):
     
     with fits.open('centeredcube_{name}.fits'.format(name=name),ignore_missing_end=True, verbose=False) as hdul:
@@ -29,10 +29,21 @@ def RotCube(name,angles):
         
     hdu_new = fits.PrimaryHDU(rot_cube)
     hdul_new = fits.HDUList([hdu_new])
-    hdul_new.writeto('derot_centeredcube_{name}'.format(name=name))
+    hdul_new.writeto('derot_centeredcube_{name}'.format(name=name)) #saves new derotated cube
     #plt.imshow(img_rot, origin='lower')
+    
+#same as above but for derotating only a single image
+def RotImage(name,angle):
+    
+    with fits.open('{name}.fits'.format(name=name),ignore_missing_end=True, verbose=False) as hdul:
+        img = hdul[0].data
+    img_rot=scipy.ndimage.rotate(img,angle)
+    hdu_new = fits.PrimaryHDU(img_rot)
+    hdul_new = fits.HDUList([hdu_new])
+    hdul_new.writeto('derot_{name}'.format(name=name)
+    plt.imshow(img_rot, origin='lower')
 
-#reads in images and takes the angles from their headers to calculate the correction angle
+#reads in header of each individual science cube frame and calculates the correction angle
 def FindAngleCorrections(name):
     
     file_names = ReadIn(name)
@@ -42,16 +53,22 @@ def FindAngleCorrections(name):
     for i in range(0, n_images):
         with fits.open('{file}'.format(file=file_names[i]),ignore_missing_end=True, verbose=False) as hdul:
             img_data = hdul[0].header
-        
-        angles[i] = img_data['ROTPOSN']-img_data['INSTANGL']
+       
+        angles[i] = img_data['ROTPOSN']-img_data['INSTANGL'] #ROTPOSN: rotator position angle, including offset INSTANGL: instrument angle
     
-    np.savetxt('{name}_rotation.txt'.format(name=name), angles)
+    #for single image, comment above 7 lines and uncomment lines below:
+                     
+    #with fits.open('{file}.fits'.format(file=name),ignore_missing_end=True, verbose=False) as hdul:
+        #img_data = hdul[0].header
+    #angles = img_data['ROTPOSN']-img_data['INSTANGL']
+    
+    np.savetxt('{name}_rotation.txt'.format(name=name), angles) #saves list of correction angles
     return angles
 
-#finding angles and derotating centred cube for each star in the file
+#main function --> loads file 'starnames.txt' containing list of stars + calls processing functions
 def FindAnglesAndDerot():
     
-    star_name=np.loadtxt('starnames.txt',dtype=str)
+    star_name=np.loadtxt('starnames.txt',dtype=str) #for single image, change to fits file name (not including '.fits' at end)
     
     n_stars=star_name.size
     for i in range(0,n_stars):
@@ -59,22 +76,8 @@ def FindAnglesAndDerot():
             name=star_name
         else:
             name=star_name[i]
-        angles=FindAngleCorrections(name)
-        RotCube(name,angles)
+        angles=FindAngleCorrections(name) #reads in fits header angle info
+        RotCube(name,angles) #derotates fits image cube by correction angle
+        #RotImage(name,angles) #derotates single fits image by correction angle
         
-FindAnglesAndDerot()
-
-"""
-#function for rotating single frames
-def RotImage(angles,images):
-    
-    for n in range(0,angles.size):
-        with fits.open('{file}'.format(file=images),ignore_missing_end=True, verbose=False) as hdul:
-            img = hdul[0].data
-        img_rot=scipy.ndimage.rotate(img,angles[n])
-        hdu_new = fits.PrimaryHDU(img_rot)
-        hdul_new = fits.HDUList([hdu_new])
-        hdul_new.writeto('derot_{name}'.format(name=images[n]))
-        plt.imshow(img_rot, origin='lower')
-"""
-      
+FindAnglesAndDerot() 
